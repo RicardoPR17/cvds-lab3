3a93df5fdd39a7db3d2fa403a3942dd67826391e
unknown <ricardo.pulido@labinfo.is.escuelaing.edu.co>
2023-02-27 10:53:04 -0500

    Clase Registry completada

--- src/main/java/edu/eci/cvds/tdd/registry/Registry.java
+++ src/main/java/edu/eci/cvds/tdd/registry/Registry.java
@@ -1,8 +1,23 @@
 package edu.eci.cvds.tdd.registry;
 
+import java.util.*;
+
 public class Registry {
+    private ArrayList<Integer> identificaciones = new ArrayList<>();
     public RegisterResult registerVoter(Person p) {
-        // TODO Validate person and return real result.
-        return RegisterResult.VALID;
+        if (p.isAlive()) {
+            if (p.getAge() <= 0) {
+                return RegisterResult.INVALID_AGE;
+            } else if (p.getAge() >= 18) {
+                if (identificaciones.contains(p.getId())) {
+                    return RegisterResult.DUPLICATED;
+                } else {
+                    identificaciones.add(p.getId());
+                    return RegisterResult.VALID;
+                }
+            }
+            return RegisterResult.UNDERAGE;
+        }
+        return RegisterResult.DEAD;
     }
 }
--- src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
+++ src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
@@ -8,9 +8,38 @@ public class RegistryTest {
 
     @Test
     public void validateRegistryResult() {
-        Person person = new Person();
+        Person person = new Person("Pepe", 123, 20, Gender.MALE, true);
         RegisterResult result = registry.registerVoter(person);
         Assert.assertEquals(RegisterResult.VALID, result);
     }
     // TODO Complete with more test cases
+    @Test
+    public void registerVoter_PersonaInvalidaMuerta_VotoNoValido() {
+        Person person = new Person("Pipo", 124, 25, Gender.MALE, false);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.DEAD, result);
+    }
+
+    @Test
+    public void registerVoter_PersonaInvalidaMenor_VotoNoValido() {
+        Person person = new Person("Juan", 125, 10, Gender.MALE, true);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.UNDERAGE, result);
+    }
+
+    @Test
+    public void registerVoter_PersonaInvalidaEdadIncorrecta_VotoNoValido() {
+        Person person = new Person("Maria", 126, -1, Gender.FEMALE, true);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.INVALID_AGE, result);
+    }
+
+    @Test
+    public void registerVoter_PersonaInvalidaIdDuplicada_VotoNoValido() {
+        Person person = new Person("Pepe", 123, 20, Gender.MALE, true);
+        Person person2 = new Person("Juana", 123, 24, Gender.FEMALE, true);
+        registry.registerVoter(person);
+        RegisterResult result = registry.registerVoter(person2);
+        Assert.assertEquals(RegisterResult.DUPLICATED, result);
+    }
 }
